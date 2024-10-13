4a65bed5c990a72e63b0217d2d3330ce031f1053
myndr1 <beln.silva@uc.cl>
2024-10-13 20:20:11 -0300

    feat(context): use context to manage views

---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+interface ProtectedRouteProps {
+    component: React.ComponentType<any>;
+    [key: string]: any;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component, ...rest }) => {
+    const { isAuthenticated } = useAuth();
+
+    return isAuthenticated ? (
+        <Component {...rest} />
+    ) : (
+        <Navigate to="/" /> 
+    );
+};
+
+export default ProtectedRoute;
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,53 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface AuthContextProps {
+  isAuthenticated: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+const validCredentials = {
+  email: 'prologin@prologin.com',
+  password: 'ProLogin123456'
+};
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
+    const storedAuth = localStorage.getItem('isAuthenticated');
+    return storedAuth ? JSON.parse(storedAuth) : false;
+  });
+
+  const login = async (email: string, password: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      if (email === validCredentials.email && password === validCredentials.password) {
+        setIsAuthenticated(true);
+        localStorage.setItem('isAuthenticated', 'true'); 
+        resolve();
+      } else {
+        setIsAuthenticated(false);
+        reject('Credenciales inválidas');
+      }
+    });
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem('isAuthenticated');
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextProps => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth debe ser usado dentro de un AuthProvider');
+  }
+  return context;
+};
