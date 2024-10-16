9bc39fbddbf6b28665a3886fc2574a1b8d11ff0e
myndr1 <beln.silva@uc.cl>
2024-10-15 22:31:58 -0300

    fixes(various): *

--- src/interfaces/interfaces.tsx
+++ src/interfaces/interfaces.tsx
@@ -10,6 +10,12 @@ export interface CommentProps {
   comment: Comment;
 }
 
+export interface AuthContextProps {
+  isAuthenticated: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
 export interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
