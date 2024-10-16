f0dcd8a2eb31df8eec805c2dfb4a67af78c3e94f
myndr1 <beln.silva@uc.cl>
2024-10-16 18:25:34 -0300

    fix(router): change manage of routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import Login from "./components/Login/Login";
-import Dashboard from "./components/Dashboard/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
-import "./assets/styles/colors.css";
+import React from 'react';
+
+import './assets/styles/colors.css';
+
+import { AuthProvider } from './context/AuthContext';
+import AppRoutes from './components/AppRoutes/AppRoutes';
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route
-            path="/dashboard"
-            element={<ProtectedRoute component={Dashboard} />}
-          />
-        </Routes>
-      </Router>
+      <AppRoutes />
     </AuthProvider>
   );
 }
