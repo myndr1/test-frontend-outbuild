a3ee9b8d691d9df2dd8df67f0a36ccbd55409010
myndr1 <beln.silva@uc.cl>
2024-10-15 22:31:50 -0300

    fix(comment-service): add try-catch

--- src/services/commentsService.tsx
+++ src/services/commentsService.tsx
@@ -6,13 +6,20 @@ export const fetchComments = async (
   page: number = 1,
   limit: number = 50,
 ): Promise<Comment[]> => {
-  console.log("apiUrl", process.env.REACT_APP_API_URL);
-  const response = await fetch(
-    `${apiUrl}/comments?_page=${page}&_limit=${limit}`,
-  );
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
+  try {
+    console.log("apiUrl", process.env.REACT_APP_API_URL);
+    const response = await fetch(
+      `${apiUrl}/comments?_page=${page}&_limit=${limit}`,
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data: Comment[] = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
   }
-  const data: Comment[] = await response.json();
-  return data;
 };
